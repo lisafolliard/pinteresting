6746a4c4061a47891276d95cf589c8cc2ee21a01
Lisa Folliard <lisaf37@gmail.com>
2015-01-03 17:32:09 -0500

    Add image upload with Paperclip part II

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211000857) do
+ActiveRecord::Schema.define(version: 20150103220258) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
